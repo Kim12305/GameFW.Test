d16b43e571ac2922c2ab7348434eeeace56abf9f
20201302 김유진 <78015118+Kim12305@users.noreply.github.com>
2021-10-28 05:45:31 +0000

    1.game class 만들기

--- main.cpp
+++ main.cpp
@@ -1,70 +1,24 @@
-#include <SDL.h>
+#include "Game.h"
 
-//윈도우 관련 정보를 저장하는 구조체
-SDL_Window* g_pWindow = 0; 
-//렌더링 상태를 포함하는 구조체
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
+Game* g_game = 0;
 
-bool init(const char* title, int xpos, int ypos,
-          int height, int width, int flags)
+int main(int argc, char* argv[])
 {
-  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  g_game = new Game();
+  g_game -> init("Game class", 100, 100, 640, 480, 0);
+
+  while(g_game->running())
   {
-    //윈도우 생성하려는 함수
-    g_pWindow = SDL_CreateWindow(title, 
-        xpos,
-        ypos,
-        height, width,
-        flags);
-
-    if(g_pWindow != 0)
-    {
-      //렌더러 생성
-      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-    }
-
+    g_game->handleEvents();
+    g_game->update();
+    g_game->render();
   }
 
-  else //실패하면 false 출력
-  {
-    return false;
-  }
-
-  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-  return true;
-}
-
-void render()
-{
-  SDL_RenderClear(g_pRenderer);
-  SDL_RenderPresent(g_pRenderer);
-}
-
-int main(int argc, char* args[])
-{
-  if(init("SDL1", 
-    SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED,
-    640, 480,
-    SDL_WINDOW_SHOWN))
-
-    g_bRunning = true;
-  
-  else return 1;
-
-  while (g_bRunning)
-  {
-    render();
-  }
-  
-  //SDL_Delay(5000); //5초대기
-  SDL_Quit();
+  g_game->clean();
 
   return 0;
-
 }
+
 
 
 
