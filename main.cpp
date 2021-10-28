9bf7378d29b79e146caac177bcffc9539764a696
20201302 김유진 <78015118+Kim12305@users.noreply.github.com>
2021-10-28 04:16:08 +0000

    덧셈

--- main.cpp
+++ main.cpp
@@ -1,33 +1,12 @@
-#include <SDL.h>
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+#include <stdio.h>
 
 int main(int argc, char* args[])
 {
- 
-   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+   int a = 0;
+   int b = 0;
 
-        g_pWindow = SDL_CreateWindow("Setting up SDL 10/22",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            640, 480, SDL_WINDOW_SHOWN);
-
-        if (g_pWindow != 0) {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-
-    }
-    else {
-        return 1;
-    }
-
-    SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
-    SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
-
-    SDL_Delay(5000);
-    SDL_Quit();
-
+   scanf("%d %d", &a, &b);
+   printf("%d", a + b);
+  
     return 0;
 }
